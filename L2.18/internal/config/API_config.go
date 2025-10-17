eafc05a109f78074f7aa3042936fcfbde618ce86
Michael <m.a.piryazev@gmail.com>
2025-10-17 21:54:24 +0300

    L2.18

---
+++ L2.18/internal/config/API_config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// APIConfig структура для настроек API
+type APIConfig struct {
+	Port string
+}
+
+func LoadAPIConfig(path string) *APIConfig {
+	err := godotenv.Load(path)
+	if err != nil {
+		log.Printf("предупреждение: .env файл не найден по пути %s, используем настройки по умолчанию", path)
+	}
+
+	return &APIConfig{
+		Port: getEnvHelper("API_PORT", "8080"),
+	}
+}
+
+func getEnvHelper(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if exists {
+		return value
+	}
+	return defaultValue
+}
