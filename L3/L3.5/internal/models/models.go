1d80c3f45800b99a4b049190d8530027f4d7150c
Michael <m.a.piryazev@gmail.com>
2025-12-04 23:40:14 +0300

    models

--- L3/L3.5/internal/models/models.go
+++ L3/L3.5/internal/models/models.go
@@ -15,3 +15,12 @@ type User struct {
 	Name    string `json:"name"`
 	IsAdmin bool   `json:"is_admin"`
 }
+
+type Booking struct {
+	ID        int64     `json:"id"`
+	EventID   int64     `json:"event_id"`
+	UserID    int64     `json:"user_id"`
+	Status    string    `json:"status"` // booked | confirmed | cancelled
+	CreatedAt time.Time `json:"created_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
